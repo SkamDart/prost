cef5e67aa37af716ac5eeefd742b1f6ab2c88b93
Cameron Dart <cdart@anduril.com>
2020-09-25 20:26:21 +0000

    Ignore basepath

--- prost-build/src/lib.rs
+++ prost-build/src/lib.rs
@@ -626,8 +626,8 @@ impl Config {
             written += subwritten;
             if subwritten != matching.len() {
                 let modname = matching[0][..=depth].join(".");
-                if let Some(buf) = basepath {
-                    self.write_line(outfile, depth + 1, &format!("include!(\"{:?}/{}.rs\");", buf, modname))?;
+                if let Some(_buf) = basepath {
+                    self.write_line(outfile, depth + 1, &format!("include!(\"{}.rs\");", modname))?;
                 } else {
                     self.write_line(outfile, depth + 1, &format!("include!(concat!(env!(\"OUT_DIR\"), \"/{}.rs\"));", modname))?;
                 }
